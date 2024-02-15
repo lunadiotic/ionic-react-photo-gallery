4b523c59a991333804dee653f5b5b5122de46cbf
aim <ahmadhakim404@gmail.com>
2024-02-15 21:11:44 +0700

    create image gallery component

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+	IonContent,
+	IonFab,
+	IonFabButton,
+	IonHeader,
+	IonIcon,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+} from '@ionic/react';
+import { camera } from 'ionicons/icons';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Gallery</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent>
+				<IonFab vertical='bottom' horizontal='center' slot='fixed'>
+					<IonFabButton
+						onClick={() => {
+							alert('clicked');
+						}}
+					>
+						<IonIcon icon={camera}></IonIcon>
+					</IonFabButton>
+				</IonFab>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Tab2;
